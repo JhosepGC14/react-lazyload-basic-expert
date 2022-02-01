45801eec00b43f48057b9b39ac0ffb130b61140a
JhosepGC14 <jhosepgc14@gmail.com>
2022-02-01 11:05:11 -0500

    eliminando pages

--- src/01-lazyload/pages/LazyPage1.tsx
+++
@@ -1,7 +0,0 @@
-export  const LazyPage1 = () => {
-  return (
-    <div>
-      <h1>LazyPage 1</h1>
-    </div>
-  );
-};
--- src/01-lazyload/pages/LazyPage2.tsx
+++
@@ -1,9 +0,0 @@
-export const LazyPage2 = () => {
-  return (
-    <div>
-      <h1>LazyPage 2</h1>
-    </div>
-  );
-};
-
-export default LazyPage2;
